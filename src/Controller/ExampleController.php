2f3702c8ef4cf4746fd1d72840c92891d44e674d
Manel <malotor@gmail.com>
2014-07-05 10:07:56 +0200

    Test render arrays

--- src/Controller/ExampleController.php
+++ src/Controller/ExampleController.php
@@ -13,9 +13,25 @@ class ExampleController {
    * Generates an example page.
    */
   public function content($name) {
-  
+  	/*
     return array(
       '#markup' => t('Hello World!'),
     );
+    */
+		return array(
+		  '#type' => 'container',
+		  '#attributes' => array('class' => array('test')),
+		  'content' => array(
+		    '#type' => 'markup',
+		    '#markup' => t('This is my test markup'),
+		    '#weight' => 0,
+		  ),
+		  'link' => array(
+		    '#type' => 'link',
+		    '#title' => t('My link'),
+		    '#href' => 'node/1',
+		    '#weight' => 1,
+		  ),
+		);
   }
 }
