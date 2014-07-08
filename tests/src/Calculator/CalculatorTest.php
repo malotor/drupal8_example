75bd54982af1ef439193adbecaf478dc0ff75801
Manel <malotor@gmail.com>
2014-07-08 20:25:09 +0200

    Prepare

---
+++ tests/src/Calculator/CalculatorTest.php
@@ -0,0 +1,42 @@
+<?php
+/*
+
+  2 + 3 = 5
+  4 + 4 = 8
+
+
+*/
+/**
+ * @file
+ * Contains Drupal\phpunit_example\Tests\CalculatorTest
+ */
+
+namespace Drupal\example\Tests;
+
+use Drupal\Tests\UnitTestCase;
+
+use Drupal\example\Calculator\Calculator;
+/**
+ * @ingroup Calculator
+ * @group Calculator
+ */
+class CalculatorTest extends UnitTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'Aritmethic Calculator Unit Test',
+      'description' => 'Aritmethic Calculator  simple unit tests',
+      'group' => 'Calculator',
+    );
+  }
+
+  public function setUp() {
+    parent::setUp();
+  }
+
+  
+
+}
