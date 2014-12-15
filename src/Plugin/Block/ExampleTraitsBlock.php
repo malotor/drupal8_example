10df0f078a6c15d30432eb169d985d0cd9162f81
Manel <malotor@gmail.com>
2014-12-15 23:39:44 +0100

    Example Traits Block added

---
+++ src/Plugin/Block/ExampleTraitsBlock.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\example\Plugin\Block\ExampleTraitsBlock.
+ */
+
+namespace Drupal\example\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+use Drupal\Core\Routing\LinkGeneratorTrait;
+use Drupal\Core\Routing\UrlGeneratorTrait;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+
+use Drupal\Core\Url;
+
+/**
+ * Provides a Example Traits Block.
+ *
+ * @Block(
+ *   id = "exampletraitsblock",
+ *   subject = @Translation("Example Traits block"),
+ *   admin_label = @Translation("Example Traits block")
+ * )
+ */
+
+class ExampleTraitsBlock extends BlockBase {
+
+  use StringTranslationTrait;
+  use LinkGeneratorTrait;
+  use UrlGeneratorTrait;
+
+  public function build() {
+
+    $items = array(
+      $this->l($this->t('Form Example') , Url::fromRoute('example.form')),
+      $this->l($this->t('User info') , Url::fromRoute('example.user_info')),
+      $this->l($this->t('Demo Custom Service') , Url::fromRoute('example.demo_service')),
+      $this->l($this->t('Secret page') , Url::fromRoute('example.secret')),
+    );
+
+    return array(
+      'menu_links' => array(
+        '#theme' => 'item_list',
+        '#items' => $items,
+      ),
+    );
+
+  }
+
+}
